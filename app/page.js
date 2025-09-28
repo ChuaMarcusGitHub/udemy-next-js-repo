67b6fcb3f60367bb07945e8b9cb79ebd55a413a4
Marcus Chua <chuamarcusanselm@gmail.com>
2025-09-28 15:56:51 +0900

    08-nextjs-app-optimizations framework

--- app/page.js
+++ app/page.js
@@ -1,18 +1,23 @@
-import Link from 'next/link';
-// Custom component
-import { RootHeader } from '@/lib/components/header';
-export default function Home() {
-	return (
-		<main>
-			<RootHeader />
+import { Suspense } from 'react';
 
-			<p>🔥 Let&apos;s get started! 🔥</p>
-			<p>
-				<Link href={'/about'}> About Us</Link>
-			</p>
-			<p>
-				<Link href={'/blog'}> Blog</Link>
-			</p>
-		</main>
-	);
+import Posts from '@/components/posts';
+import { getPosts } from '@/lib/posts';
+
+async function LatestPosts() {
+  const latestPosts = await getPosts(2);
+  return <Posts posts={latestPosts} />;
+}
+
+export default async function Home() {
+  return (
+    <>
+      <h1>Welcome back!</h1>
+      <p>Here's what you might've missed.</p>
+      <section id="latest-posts">
+      <Suspense fallback={<p>Loading recent posts...</p>}>
+        <LatestPosts />
+      </Suspense>
+      </section>
+    </>
+  );
 }

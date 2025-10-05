9ea1036e65981c4b2013c1c65e8104f345522f7d
Marcus Chua <chuamarcusanselm@gmail.com>
2025-10-05 16:16:22 +0900

    [nextjs-app-optimizations] Adding Metadata

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,19 @@
 import Header from '@/components/header';
 import './globals.css';
 
+// propagates to child pages
 export const metadata = {
-  title: 'NextPosts',
-  description: 'Browse and share amazing posts.',
+	title: 'NextPosts',
+	description: 'Browse and share amazing posts.',
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Header />
-        <main>{children}</main>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<Header />
+				<main>{children}</main>
+			</body>
+		</html>
+	);
 }
